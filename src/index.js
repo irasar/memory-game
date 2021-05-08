1de5bcc1252ccccd641faaa53aa85a04f7e1da1a
Sarah Iraurgui <iraurguisj@gmail.com>
2021-05-08 17:05:37 -0400

    array object for diff cards created

---
+++ src/index.js
@@ -0,0 +1,27 @@
+//card options
+const cards = [
+    {
+        name: "fries",
+        img: "src/images/fries.png"
+    },
+    {
+        name: "cheeseburger",
+        img: "src/images/cheeseburger.png"
+    },
+    {
+        name: "ice-cream",
+        img: "src/images/ice-cream.png"
+    },
+    {
+        name: "pizza",
+        img: "src/images/pizza.png"
+    },
+    {
+        name: "milkshake",
+        img: "src/images/milkshake.png"
+    },
+    {
+        name: "hotdog",
+        img: "src/images/hotdog.png"
+    },
+]
